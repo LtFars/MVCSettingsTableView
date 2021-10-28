3cf3ebcff835cbc03739c7cfcbbc7257f9f53043
Denis Snezhko <mikle4421@mail.ru>
2021-10-28 19:22:18 +0300

    Create model structure and configure model data.

--- MVCSettingsTableView/Sources/Scenes/SettingsModel/SettingsModel.swift
+++ MVCSettingsTableView/Sources/Scenes/SettingsModel/SettingsModel.swift
@@ -7,6 +7,21 @@
 
 import UIKit
 
-class SettingsModel {
+final class SettingsModel {
     
+    func createOptions() -> [Section] {
+        return [
+            
+            // First section
+            Section(title: "", option: [
+                SettingsCellOption(title: "Авиарежим", additionLabel: "", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemOrange, isSwitchButton: true),
+                SettingsCellOption(title: "Wi-Fi", additionLabel: "Выкл", icon: UIImage(systemName: "wifi"), iconBackgroundColor: .systemBlue, isSwitchButton: false),
+                SettingsCellOption(title: "Bluetooth", additionLabel: "Вкл.", icon: UIImage(systemName: "iphone.homebutton.radiowaves.left.and.right"), iconBackgroundColor: .systemBlue, isSwitchButton: false),
+                SettingsCellOption(title: "Сотовая связь", additionLabel: "", icon: UIImage(systemName: "antenna.radiowaves.left.and.right"), iconBackgroundColor: .systemGreen, isSwitchButton: false),
+                SettingsCellOption(title: "Режим модема", additionLabel: "", icon: UIImage(systemName: "personalhotspot"), iconBackgroundColor: .systemGreen, isSwitchButton: false),
+                SettingsCellOption(title: "VPN", additionLabel: "", icon: UIImage(systemName: "personalhotspot"), iconBackgroundColor: .systemBlue, isSwitchButton: true)
+                
+            ])
+        ]
+    }
 }
